611394a62df5a18404a185c226badf5ed6ef53df
bartoszluka <bartoszluka1@gmail.com>
2022-02-11 00:17:24 +0100

    Change day 6 implementation to use arrays

--- src/Day06.hs
+++ src/Day06.hs
@@ -1,56 +1,39 @@
 module Day06 (part1, part2) where
 
 import Control.Monad (liftM2)
+import Data.Array (Array)
+import Data.Array.IArray (
+    Array,
+    accumArray,
+    elems,
+    ixmap,
+    (!),
+    (//),
+ )
 import Parsing (parseInput, runParse)
 
-data SimulationDay = SimulationDay
-    { counter0 :: Int
-    , counter1 :: Int
-    , counter2 :: Int
-    , counter3 :: Int
-    , counter4 :: Int
-    , counter5 :: Int
-    , counter6 :: Int
-    , counter7 :: Int
-    , counter8 :: Int
-    }
-    deriving (Show, Eq)
+type SimulationDay = Array Int Int
 
 nextDay :: SimulationDay -> SimulationDay
-nextDay sd =
-    SimulationDay
-        { counter0 = counter1 sd
-        , counter1 = counter2 sd
-        , counter2 = counter3 sd
-        , counter3 = counter4 sd
-        , counter4 = counter5 sd
-        , counter5 = counter6 sd
-        , counter6 = counter7 sd + counter0 sd
-        , counter7 = counter8 sd
-        , counter8 = counter0 sd
-        }
+nextDay = updateSixth . ixmap (0, 8) (decMod 9)
+  where
+    decMod maxValue n = (n + 1) `mod` maxValue
+    updateSixth arr = arr // [(6, arr ! 6 + arr ! 8)]
 
-fromList :: (Num a, Eq a) => [a] -> SimulationDay
-fromList list =
-    let c n = length . filter (== n) $ list
-     in SimulationDay (c 0) (c 1) (c 2) (c 3) (c 4) (c 5) (c 6) (c 7) (c 8)
+fromList :: [Int] -> SimulationDay
+fromList =
+    accumArray increment 0 (0, 8) . map duplicate
+  where
+    duplicate a = (a, a)
+    increment n _ = n + 1
 
 fishCount :: SimulationDay -> Int
-fishCount sd =
-    counter0 sd
-        + counter1 sd
-        + counter2 sd
-        + counter3 sd
-        + counter4 sd
-        + counter5 sd
-        + counter6 sd
-        + counter7 sd
-        + counter8 sd
+fishCount = sum . elems
 
 simulateFish :: SimulationDay -> [SimulationDay]
 simulateFish currentDay = currentDay : simulateFish (nextDay currentDay)
 
-done :: (Num a, Eq a) => Int -> [a] -> Int
+done :: Int -> [Int] -> Int
 done day = fishCount . nth day . simulateFish . fromList
 
 done6parsing :: String -> Maybe Int
