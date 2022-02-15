b3a0030b2c87a08891f352cb327d73e219389ab4
bartoszluka <bartoszluka1@gmail.com>
2022-02-15 03:01:54 +0100

    Remove unused functions

--- src/Parsing.hs
+++ src/Parsing.hs
@@ -12,15 +12,11 @@ import Text.Parsec (
     string,
  )
 import Text.Parsec.String (Parser)
-import Text.Read (Read (readList))
 
 integer :: Parser Int
 integer = do
     n <- many1 digit
     return (read n)
-
-parseInput :: Parser [Int]
-parseInput = sepBy integer (char ',')
 
 runParse :: Parser a -> String -> Either ParseError a
 runParse p = parse p ""
